99366735f025561083dd3da857e6bff5e32fc2e0
Ivan <offlane@tut.by>
2022-10-31 21:03:58 +0300

    Типизация стейта

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface TodoItemProps {
   id: string;
@@ -6,7 +6,17 @@ interface TodoItemProps {
   completed: boolean;
   style?: React.CSSProperties;
 }
+
+type Todo = {
+  id: string;
+};
+
 const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed, style = {} }) => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<string[]>([]);
+  const [] = useState<string[] | null>(null);
+  const [] = useState<Todo | null>(null);
+
   return (
     <li style={{ color: 'teal', backgroundColor: 'white', ...style }}>
       <input type="checkbox" checked={completed} />
