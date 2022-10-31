77c61c85b1dd5f3e988ac2e71c7a4c235865d547
Ivan <offlane@tut.by>
2022-10-31 20:48:54 +0300

    Типизация пропсов

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface TodoItemProps {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed }) => {
+  return (
+    <li>
+      <input type="checkbox" checked={completed} />
+      <span>{title}</span>
+      <span>&times;</span>
+    </li>
+  );
+};
+
+export default TodoItem;
